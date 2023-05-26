2f9aa73f97146468f9fa94f71d72282a6d842f59
ovilkovsky <ovilkovsky@unitedthinkers.com>
2023-05-26 17:11:57 +0300

    1

--- curl_1.py
+++ curl_1.py
@@ -3,9 +3,9 @@ from urllib.parse import urlencode
 
 c = pycurl.Curl()
 #initializing the request URL
-c.setopt(c.URL, 'https://httpbin.org/post')
+c.setopt(c.URL, 'https://admin-api-qa-1.horeker.dev/admin/auth/login')
 #the data that we need to Post
-post_data = {'field': 'value'}
+post_data = {"email":"admin@mail.test","password":"admin1"}
 # encoding the string to be used as a query
 postfields = urlencode(post_data)
 #setting the cURL for POST operation
